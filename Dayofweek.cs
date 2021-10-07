d228677f1ad1544e64745c64c492028fa42bcb08
syedfaizz <syedfaizz45@gmail.com>
2021-10-07 10:14:13 +0530

    [faisal] Add: welcome

---
+++ Dayofweek.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DayOfTheWeek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t Program to get Day Of the Week After Specified Days");
+            Console.WriteLine("\t ==================================================================");
+            Console.Write("\t Enter Days After Today : ");
+            int days = Convert.ToInt32(Console.ReadLine());
+            DateTime date = DateTime.Now.AddDays(days);
+            Console.WriteLine("\t Current Date : " + DateTime.Now.ToString("MM/dd/yyyy"));
+            Console.WriteLine("\t Date After {0} Days : {1}", days, date.ToString("MM/dd/yyyy"));
+            Console.WriteLine("\t Day Of The Week On {0} : {1}", date.ToString("MM/dd/yyyy"), date.DayOfWeek);
+            Console.ReadKey();
+        }
+    }
+}
